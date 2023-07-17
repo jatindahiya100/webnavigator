1cdd83290596466c4c9e41efa2679eefb641d8be
Jatin Dahiya <jatindahiya100@gmail.com>
2023-07-17 14:36:48 +0530

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../assets/css/header.css'
 import logo from '../assets/images/logo.png'
+import logo1 from '../assets/images/thrive-logo.png'
 import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
@@ -9,19 +10,20 @@ export default function Header() {
     <div className='header'>
 
       <div className="logo">
-        <Link to='/'><img className='logo' src={logo} alt="" /></Link>
+        <Link to='/'><img className='logo' src={logo1} alt="" /></Link>
       </div>
 
       <div className="nav-items">
-        <NavLink className="primary-text-light-mode" to='/'>Home</NavLink>
-        <NavLink className="primary-text-light-mode" to='/about'>About</NavLink>
-        <NavLink className="primary-text-light-mode" to='/work'>Work</NavLink>
-        <NavLink className="primary-text-light-mode" to='/services'>Services</NavLink>
+        <NavLink className="primary-text-light-mode" to='/'><ion-icon name="home"></ion-icon>Home</NavLink>
+        <NavLink className="primary-text-light-mode" to='/services'>What We Do</NavLink>
+        <NavLink className="primary-text-light-mode" to='/about'>We Are</NavLink>
+        <NavLink className="primary-text-light-mode" to='/work'>Portfolio</NavLink>
+        <NavLink className="primary-text-light-mode" to='/contact'>Contact</NavLink>
       </div>
 
 
       <div className="quick-actions">
-
+        <div className='toogle-mode'><ion-icon name="moon"></ion-icon></div>
       </div>
 
     </div>
