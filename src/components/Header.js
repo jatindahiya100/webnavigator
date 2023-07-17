7fb77bb1ab808d310cb1e761afb27cfd955a4b23
Jatin Dahiya <jatindahiya100@gmail.com>
2023-07-18 00:44:19 +0530

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../assets/css/Header.css'
 import logo from '../assets/images/thrive-logo.png'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Header() {
 
+  const [isNavOpen, setNavOpen] = useState(false);
+
+  window.addEventListener("resize", function () {
+    if (window.innerWidth >= 800) {
+      var menu = document.querySelector("nav");
+      menu.className = "desktop-menu";
+      setNavOpen(false);
+    }
+  })
+
+  const toggleMenu = () => {
+    setNavOpen(!isNavOpen);
+  }
+
   return (
-    <div className='header'>
-    </div>
+    <header>
+
+      <div className="logo">
+        <Link to='/'><img src={logo} alt="" /></Link>
+      </div>
+
+      <nav className={isNavOpen === true ? 'mobile-menu' : 'desktop-menu'}>
+        <NavLink to='/' onClick={isNavOpen === true ? toggleMenu : ''}><ion-icon name="home"></ion-icon> Home</NavLink>
+        <NavLink to='/services' onClick={isNavOpen === true ? toggleMenu : ''}><ion-icon name="shapes"></ion-icon> Services</NavLink>
+        <NavLink to='/work' onClick={isNavOpen === true ? toggleMenu : ''}><ion-icon name="ribbon"></ion-icon> Work</NavLink>
+        <NavLink to='/about' onClick={isNavOpen === true ? toggleMenu : ''}><ion-icon name="logo-react"></ion-icon> About</NavLink>
+        <NavLink to='/startProject' onClick={isNavOpen === true ? toggleMenu : ''}><ion-icon name="infinite"></ion-icon> Start Project</NavLink>
+      </nav>
+
+      <div className='toggle-menu' onClick={toggleMenu}><ion-icon name="menu"></ion-icon></div>
+
+    </header>
   )
 }
