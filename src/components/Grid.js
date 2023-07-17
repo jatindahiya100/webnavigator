d25fbda693b6f462278d8270a111612d15d630e4
Jatin Dahiya <jatindahiya100@gmail.com>
2023-07-17 16:25:44 +0530

    updated

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,23 +2,42 @@ import React from 'react';
 import '../assets/css/Grid.css';
 import Lottie from 'lottie-react'
 
-const TextImageGrid = ({ heading, text, btnText, btnLink, imageType, imageUrl }) => {
+const Grid = ({ index, heading, text, btnText, btnLink, imageType, imageUrl }) => {
+  const isEven = index % 2 === 0;
+
+  const renderTextItem = () => (
+    <div className="grid-item text">
+      <h1 className='primary-text-light-mode'>{heading}</h1>
+      <p className='secondary-text-light-mode'>{text}</p>
+      <button className='button' onClick={() => { window.open({ btnLink }) }}>{btnText} <ion-icon name="arrow-down-circle"></ion-icon></button>
+    </div>
+  );
+
+  const renderImageItem = () => (
+    <div className="grid-item">
+      {imageType === "Lottie" ?
+        <Lottie className='image' animationData={imageUrl} />
+        :
+        <img className="image" src={imageUrl} alt="" />
+      }
+    </div>
+  );
+
   return (
     <div className="grid-container">
-      <div className="grid-item text">
-        <h1 className='primary-text-light-mode'>{heading}</h1>
-        <p className='secondary-text-light-mode'>{text}</p>
-        <button className='button' onClick={() => { window.open({ btnLink }) }}>{btnText} <ion-icon name="arrow-down-circle"></ion-icon></button>
-      </div>
-      <div className="grid-item">
-        {imageType === "Lottie" ?
-          <Lottie className='image' animationData={imageUrl} />
-          :
-          <img className="image" src={imageUrl} alt="" />
-        }
-      </div>
+      {isEven ? (
+        <>
+          {renderTextItem()}
+          {renderImageItem()}
+        </>
+      ) : (
+        <>
+          {renderImageItem()}
+          {renderTextItem()}
+        </>
+      )}
     </div>
   );
 };
 
-export default TextImageGrid;
+export default Grid;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,18 +12,37 @@ export default function Home() {
       imageType: "Lottie",
       imageUrl: animationData
     },
+    {
+      heading: "I’m Jatin Dahiya. I live in Sonipat City, where I design the future.",
+      text: "I’ve loved making things for as long as I can remember, and wrote my first program when I was 12 years old, just two weeks after my father brought home the brand new DELL Latitude | E5410 that I taught myself to type on.",
+      btnText: "Explore More",
+      btnLink: "",
+      imageType: "Lottie",
+      imageUrl: animationData
+    },
     // Add more grid data objects as needed
   ];
 
   return (
     <>
       <Grid
+        index={0}
         heading={gridData[0].heading}
         text={gridData[0].text}
         btnText={gridData[0].btnText}
         btnLink={gridData[0].btnLink}
         imageType={gridData[0].imageType}
         imageUrl={gridData[0].imageUrl}
+      />
+
+<Grid
+        index={1}
+        heading={gridData[1].heading}
+        text={gridData[1].text}
+        btnText={gridData[1].btnText}
+        btnLink={gridData[1].btnLink}
+        imageType={gridData[1].imageType}
+        imageUrl={gridData[1].imageUrl}
       />
 
       {/* Render other components here */}
