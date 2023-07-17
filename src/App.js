6ca132f84955f8e9b8b3163c5483ee0eed515227
Jatin Dahiya <jatindahiya100@gmail.com>
2023-07-17 14:07:12 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Header from './components/Header'
+import About from './pages/About';
+import Home from './pages/Home';
+import { HashRouter as Router, Routes, Route, } from "react-router-dom";
+import Work from './pages/Work';
+import Services from './pages/Services';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/work' element={<Work />} />
+        <Route path='/services' element={<Services />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import '../assets/css/header.css'
+import logo from '../assets/images/logo.png'
+import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
+
   return (
-    <div>Header</div>
+    <div className='header'>
+
+      <div className="logo">
+        <Link to='/'><img className='logo' src={logo} alt="" /></Link>
+      </div>
+
+      <div className="nav-items">
+        <NavLink className="primary-text-light-mode" to='/'>Home</NavLink>
+        <NavLink className="primary-text-light-mode" to='/about'>About</NavLink>
+        <NavLink className="primary-text-light-mode" to='/work'>Work</NavLink>
+        <NavLink className="primary-text-light-mode" to='/services'>Services</NavLink>
+      </div>
+
+
+      <div className="quick-actions">
+
+      </div>
+
+    </div>
   )
 }
