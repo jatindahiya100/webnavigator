a69ec0487e73ca29f014e6509a6c8b8f14a0ef2c
Jatin Dahiya <jatindahiya100@gmail.com>
2023-07-17 16:10:57 +0530

    updated

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import '../assets/css/TextImageGrid.css';
+import '../assets/css/Grid.css';
 import Lottie from 'lottie-react'
 
 const TextImageGrid = ({ heading, text, btnText, btnLink, imageType, imageUrl }) => {
   return (
-    <div className="text-image-grid">
-      <div className="text-content">
+    <div className="grid-container">
+      <div className="grid-item text">
         <h1 className='primary-text-light-mode'>{heading}</h1>
         <p className='secondary-text-light-mode'>{text}</p>
         <button className='button' onClick={() => { window.open({ btnLink }) }}>{btnText} <ion-icon name="arrow-down-circle"></ion-icon></button>
       </div>
-      <div className="image-content">
+      <div className="grid-item">
         {imageType === "Lottie" ?
-          <Lottie animationData={imageUrl} />
+          <Lottie className='image' animationData={imageUrl} />
           :
-          <img src={imageUrl} alt="" className="image" />
+          <img className="image" src={imageUrl} alt="" />
         }
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import TextImageGrid from '../components/TextImageGrid'
-import animationData from '../assets/images/Animation1689573956186.json'
+import React from 'react';
+import Grid from '../components/Grid';
+import animationData from '../assets/images/Animation1689573956186.json';
 
 export default function Home() {
+  const gridData = [
+    {
+      heading: "I’m Jatin Dahiya. I live in Sonipat City, where I design the future.",
+      text: "I’ve loved making things for as long as I can remember, and wrote my first program when I was 12 years old, just two weeks after my father brought home the brand new DELL Latitude | E5410 that I taught myself to type on.",
+      btnText: "Explore More",
+      btnLink: "",
+      imageType: "Lottie",
+      imageUrl: animationData
+    },
+    // Add more grid data objects as needed
+  ];
+
   return (
     <>
-      <TextImageGrid
-        heading="I’m Jatin Dahiya. I live in Sonipat City, where I design the future."
-        text="I’ve loved making things for as long as I can remember, and wrote my first program when I was 12 years old, just two weeks after my father brought home the brand new DELL Latitude | E5410 that I taught myself to type on."
-        btnText="Explore More"
-        btnLink=""
-        imageType="Lottie"
-        imageUrl={animationData}
+      <Grid
+        heading={gridData[0].heading}
+        text={gridData[0].text}
+        btnText={gridData[0].btnText}
+        btnLink={gridData[0].btnLink}
+        imageType={gridData[0].imageType}
+        imageUrl={gridData[0].imageUrl}
       />
+
+      {/* Render other components here */}
+
     </>
-  )
+  );
 }
