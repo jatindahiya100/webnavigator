7dd4e49c50fcc015401c7e19c57051400130d165
Jatin Dahiya <jatindahiya100@gmail.com>
2023-07-17 23:11:16 +0530

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,11 @@
 import React from 'react'
 import '../assets/css/Header.css'
 import logo from '../assets/images/thrive-logo.png'
-import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
 
   return (
     <div className='header'>
-
-      <div className="logo">
-        <Link to='/'><img className='logo' src={logo} alt="" /></Link>
-      </div>
-
-      <div className="nav-items">
-        <NavLink className="primary-text-light-mode" to='/'><ion-icon name="home"></ion-icon>Home</NavLink>
-        <NavLink className="primary-text-light-mode" to='/services'><ion-icon name="cube"></ion-icon> Services</NavLink>
-        <NavLink className="primary-text-light-mode" to='/work'><ion-icon name="logo-react"></ion-icon> Work</NavLink>
-        <NavLink className="primary-text-light-mode" to='/about'><ion-icon name="information-circle"></ion-icon> About</NavLink>
-        <NavLink className="primary-text-light-mode" to='/contact'><ion-icon name="infinite"></ion-icon> Start Project</NavLink>
-      </div>
-
-
-      <div className="quick-actions">
-        <div className='toogle-mode'><ion-icon name="moon"></ion-icon></div>
-      </div>
-
     </div>
   )
 }
