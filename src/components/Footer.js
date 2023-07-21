e1ecdcc243785892c98f97f6b8b069d3fcf43afe
Jatin Dahiya <jatindahiya100@gmail.com>
2023-07-21 08:19:05 +0530

    Update Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ export default function Footer() {
             <li><NavLink to='/about'><ion-icon name="cube"></ion-icon> About</NavLink></li>
           </ul>
         </div>
-        <p style={{ width: "60%", textAlign: "center", lineHeight: "2em" }}>
+        <p>
           Made With <span style={{ color: "crimson" }}><ion-icon name="heart"></ion-icon></span> In India. All rights reserved &copy; 2023.
           Let's collaborate and create exceptional digital solutions that reflect the rich heritage and collective brilliance of our global community. Together, we can build a brighter digital future!
         </p>
