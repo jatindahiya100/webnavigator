532453a22761793b66e039b1ad423c8979057ac1
Jatin Dahiya <jatindahiya100@gmail.com>
2023-07-30 12:24:55 +0530

    Update Changes

--- src/components/TwoColumnGrid.js
+++ src/components/TwoColumnGrid.js
@@ -4,18 +4,20 @@ import Card from '../components/Card'
 
 const TwoColumnGrid = ({ items }) => {
     return (
-        <div className="two-column-grid-container">
-            {items.map((item, index) => (
-                <div className="two-column-grid-item" key={index}>
-                    <Card
-                        imageSrc={item.imageSrc}
-                        heading={item.heading}
-                        description={item.description}
-                        buttonText={item.buttonText}
-                        websiteUrl={item.websiteUrl}
-                    />
-                </div>
-            ))}
+        <div className="component-wrapper">
+            <div className="two-column-grid-container">
+                {items.map((item, index) => (
+                    <div className="two-column-grid-item" key={index}>
+                        <Card
+                            imageSrc={item.imageSrc}
+                            heading={item.heading}
+                            description={item.description}
+                            buttonText={item.buttonText}
+                            websiteUrl={item.websiteUrl}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
