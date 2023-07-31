0b74cb8350ed8e763e8d7619eff50d7512852e1e
Jatin Dahiya <jatindahiya100@gmail.com>
2023-07-31 15:06:59 +0530

    Update Changes

--- src/components/ComponentGrid.js
+++ src/components/ComponentGrid.js
@@ -2,7 +2,11 @@ import React from 'react';
 import '../assets/css/ComponentGrid.css';
 
 const ComponentGrid = ({ children }) => {
-  return <div className="grid-container">{children}</div>;
+  return (
+    <div className='grid-wrapper'>
+      <div className="grid-container">{children}</div>
+    </div>
+  );
 };
 
 export default ComponentGrid;
