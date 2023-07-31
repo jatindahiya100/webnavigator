5ac46ced3489a01625540aa61d1944cee4c453fb
Jatin Dahiya <jatindahiya100@gmail.com>
2023-07-31 13:21:16 +0530

    Update Changes

---
+++ src/components/ComponentGrid.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/css/ComponentGrid.css';
+
+const ComponentGrid = ({ children }) => {
+  return <div className="grid-container">{children}</div>;
+};
+
+export default ComponentGrid;
---
+++ src/components/MediaComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Lottie from 'lottie-react'; // Make sure to install 'react-lottie' npm package
+
+const MediaComponent = ({ mediaType, imageUrl, lottieOptions }) => {
+    const isImage = mediaType === 'image';
+    const isLottie = mediaType === 'lottie';
+
+    return (
+        <div>
+            {isImage && imageUrl && <img src={imageUrl} alt="Image" />}
+            {isLottie && lottieOptions && <Lottie animationData={lottieOptions.animationData} />}
+        </div>
+    );
+};
+
+export default MediaComponent;
