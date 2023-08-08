f30c209884341083ea5210d97d983fc953524092
Jatin Dahiya <jatindahiya100@gmail.com>
2023-08-08 17:29:54 +0530

    Update Changes

--- src/components/FullBlogPost.js
+++ src/components/FullBlogPost.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useParams } from 'react-router-dom'; // Import useParams to get the post id from the URL
+import { useParams } from 'react-router-dom';
 import mockBlogPosts from '../components/mockBlogPosts';
-
+import '../assets/css/FullBlogPost.css'; // Import the CSS file
 
 function FullBlogPost() {
-    const { postId } = useParams(); // Get the post id from the URL
+    const { postId } = useParams();
     const selectedPost = mockBlogPosts.find(post => post.id === parseInt(postId, 10));
 
     if (!selectedPost) {
@@ -12,10 +12,10 @@ function FullBlogPost() {
     }
 
     return (
-        <div>
-            <img src={selectedPost.featuredImage} alt="Featured" />
-            <h2>{selectedPost.heading}</h2>
-            {selectedPost.mainContent}
+        <div className="blog-container">
+            <img src={selectedPost.featuredImage} alt="Featured" className="blog-featured-image" />
+            <h2 className="blog-heading">{selectedPost.heading}</h2>
+            <div className="blog-main-content">{selectedPost.mainContent}</div>
         </div>
     );
 }
