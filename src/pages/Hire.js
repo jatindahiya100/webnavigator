f498930b63027a80874f230d04e50446172c17e3
Jatin Dahiya <jatindahiya100@gmail.com>
2023-08-10 11:21:45 +0530

    Update Changes

--- src/pages/Hire.js
+++ src/pages/Hire.js
@@ -5,8 +5,6 @@ import CustomComponent from '../components/CustomComponent'
 import MediaComponent from '../components/MediaComponent'
 // Lottie Imports
 import ChatLottie from '../assets/images/Lottie/Chat-Lottie-01.json';
-import HireOnUpworkLottie from '../assets/images/Lottie/Hire-On-Upwork-Lottie-01.json';
-import HowToHireOnUpworkLottie from '../assets/images/Lottie/How-To-Hire-On-Upwork-Lottie-01.json';
 import { useEffect } from 'react';
 
 export default function Hire() {
@@ -34,57 +32,6 @@ export default function Hire() {
                     loop: true,
                     autoplay: true,
                     animationData: ChatLottie,
-                },
-            },
-        },
-        {
-            CustomComponent: {
-                gravity: "low",
-                title: "Hire Us on Upwork for Secure Payments and More!",
-                description: "",
-                listItems: [
-                    "Secure Payments: Reliable and worry-free transactions on Upwork.",
-                    "Verified Reviews: Authentic client feedback for quality assurance.",
-                    "Escrow Protection: Ensured fairness through milestone-based payments.",
-                    "Time-Tracking: Transparent progress monitoring with Upwork's feature.",
-                ],
-                buttonText: "Hire On Upwork",
-                onClick: () => {
-                    window.open("https://www.upwork.com/ab/flservices/workwith/jatindahiya");
-                },
-            },
-            MediaComponent: {
-                mediaType: "lottie",
-                lottieOptions: {
-                    loop: true,
-                    autoplay: true,
-                    animationData: HireOnUpworkLottie,
-                },
-            },
-        },
-        {
-            CustomComponent: {
-                gravity: "low",
-                title: "How to Hire Us on Upwork?",
-                description: "",
-                listItems: [
-                    "Visit our Upwork profile by clicking the link below.",
-                    "Review our portfolio, client feedback, and the range of services we offer.",
-                    "Share your project requirements, and we will promptly respond with a tailored proposal.",
-                    "Fund the project securely through Upwork's escrow system.",
-                    "Sit back and relax as we bring your digital vision to life!",
-                ],
-                buttonText: "Upwork Profile",
-                onClick: () => {
-                    window.open("https://www.upwork.com/freelancers/~012d6eb926a1f7a86a");
-                },
-            },
-            MediaComponent: {
-                mediaType: "lottie",
-                lottieOptions: {
-                    loop: true,
-                    autoplay: true,
-                    animationData: HowToHireOnUpworkLottie,
                 },
             },
         },
