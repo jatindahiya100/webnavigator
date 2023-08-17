3387bb3f8175ebd7e3dce14cc92aef8f3c5e2409
Jatin Dahiya <jatindahiya100@gmail.com>
2023-08-17 13:28:29 +0530

    Update Changes

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Lottie from 'lottie-react'; // Make sure to install 'react-lottie' npm package
+import notFoundImage from '../assets/images/Lottie/Not-Found-Lottie-01.json'; // Replace with your actual image path
 
-export default function NotFound() {
-    return (
-        <div>NotFound</div>
-    )
-}
+const NotFound = () => {
+  return (
+    <div style={styles.container}>
+      <Lottie animationData={notFoundImage} style={{height: "80vh"}} />
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+};
+
+export default NotFound;
