2b05f2f08b522d0b8067e1f0206a51460d2e35de
Jatin Dahiya <jatindahiya100@gmail.com>
2023-08-31 13:44:46 +0530

    Update Changes

--- src/blogs/BlogPostCard.js
+++ src/blogs/BlogPostCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import '../assets/css/BlogPostCard.css';
 
 function BlogPostCard({ featuredImage, heading, shortDescription }) {
   return (
     <div className="blog-post-card">
-      <img src={featuredImage} alt="Blog Post" className="thumbnail" />
+      <LazyLoadImage
+        src={featuredImage}
+        alt="Blog Post"
+        className="thumbnail"
+        effect="blur" // You can choose other effects as well
+      />
       <div className="content">
         <h2 className="heading">{heading}</h2>
         <p className="short-description">{shortDescription}</p>
