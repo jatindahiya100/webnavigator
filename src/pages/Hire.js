4eeb4da5f709928b29dbea78589d5c472e244f6b
Jatin Dahiya <jatindahiya100@gmail.com>
2023-09-05 01:28:50 +0530

    Update Changes

--- src/pages/Hire.js
+++ src/pages/Hire.js
@@ -1,10 +1,5 @@
 import React from 'react'
-// Components Imports
-import ComponentGrid from '../components/ComponentGrid';
-import CustomComponent from '../components/CustomComponent'
-import MediaComponent from '../components/MediaComponent'
 // Lottie Imports
-import ChatLottie from '../assets/images/Lottie/Chat-Lottie-01.json';
 import { useEffect } from 'react';
 import Banner from '../components/Banner';
 
@@ -13,66 +8,9 @@ export default function Hire() {
         document.title = "The Web Navigators | Start Project";
     }, [])
 
-    const gridData = [
-        {
-            CustomComponent: {
-                gravity: "heavy",
-                title: "Ready to Get Started? Shoot Us a Chat!",
-                description:
-                    "Are you in need of top-notch digital solutions for your business? Look no further! At Web Navigators, we are committed to crafting digital experiences that drive success for your brand. Whether you require a stunning custom website, an SEO strategy that boosts your online presence, captivating social media management, or powerful ads campaigns, we've got you covered.",
-                listItems: [],
-                buttonText: "Get in Touch",
-                onClick: () => {
-                    window.tidioChatApi.show();
-                    window.tidioChatApi.open();
-                },
-            },
-            MediaComponent: {
-                mediaType: "lottie",
-                lottieOptions: {
-                    loop: true,
-                    autoplay: true,
-                    animationData: ChatLottie,
-                },
-            },
-        },
-        // Add more grid data objects as needed
-    ];
-
-
-    const isMobileDevice = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        window.navigator.userAgent
-    );
-
     return (
         <>
             <Banner/>
-            <ComponentGrid>
-                {gridData.map((item, index) => (
-                    <React.Fragment key={index}>
-                        {isMobileDevice ? (
-                            // Render CustomComponent and MediaComponent in the same order for mobile devices
-                            <>
-                                <CustomComponent {...item.CustomComponent} />
-                                <MediaComponent {...item.MediaComponent} />
-                            </>
-                        ) : (
-                            // Alternate the order of rendering for non-mobile devices
-                            index % 2 === 0 ? (
-                                <>
-                                    <CustomComponent {...item.CustomComponent} />
-                                    <MediaComponent {...item.MediaComponent} />
-                                </>
-                            ) : (
-                                <>
-                                    <MediaComponent {...item.MediaComponent} />
-                                    <CustomComponent {...item.CustomComponent} />
-                                </>
-                            )
-                        )}
-                    </React.Fragment>
-                ))}
-            </ComponentGrid>
         </>
     );
 }
--- src/pages/WhatWeDo.js
+++ src/pages/WhatWeDo.js
@@ -1,12 +1,4 @@
 import React from 'react'
-// Components Imports
-import ComponentGrid from '../components/ComponentGrid';
-import CustomComponent from '../components/CustomComponent'
-import MediaComponent from '../components/MediaComponent'
-// Images Import
-import WebDesignCover from '../assets/images/what-we-do/web-design-cover.png'
-import DigitalMarketingCover from '../assets/images/what-we-do/digital-marketing-cover.jpg'
-import HostingCover from '../assets/images/what-we-do/hosting-cover.png'
 import { useEffect } from 'react';
 import Banner from '../components/Banner';
 
@@ -15,94 +7,9 @@ export default function WhatWeDo() {
     document.title = "The Web Navigators | What We Do";
   }, [])
 
-  const gridData = [
-    {
-      CustomComponent: {
-        gravity: "heavy",
-        title: "Web Design & Development",
-        listItems: [
-          "Custom Website Design: Tailored designs to represent your brand and meet your specific needs.",
-          "Responsive Web Development: Creating websites that adapt to various devices and screen sizes.",
-          "E-commerce Solutions: Building online stores with secure payment gateways and user-friendly interfaces.",
-          "Content Management Systems (CMS): Implementing systems like WordPress, Joomla, or Drupal for easy website management.",
-          "Website Maintenance: Ensuring your website stays up-to-date, secure, and optimized for peak performance.",
-        ],
-      },
-      MediaComponent: {
-        mediaType: "image",
-        imageUrl: WebDesignCover,
-      },
-    },
-    {
-      CustomComponent: {
-        gravity: "low",
-        title: "Digital Marketing",
-        listItems: [
-          "Paid Advertising (PPC): Creating and managing effective pay-per-click campaigns to drive targeted traffic.",
-          "Search Engine Optimization (SEO): Optimizing websites to improve search engine rankings and organic visibility.",
-          "Social Media Marketing: Leveraging social platforms to engage with audiences and build brand awareness.",
-          "Email Marketing: Crafting compelling email campaigns to nurture leads and drive conversions.",
-          "Analytics and Conversion Tracking: Monitoring and analyzing website data to enhance marketing strategies.",
-        ],
-      },
-      MediaComponent: {
-        mediaType: "image",
-        imageUrl: DigitalMarketingCover,
-      },
-    },
-    {
-      CustomComponent: {
-        gravity: "low",
-        title: "Domain & Hosting Services",
-        listItems: [
-          "Domain Registration: Assisting clients in finding and registering suitable domain names for their websites.",
-          "Web Hosting: Providing reliable hosting solutions with optimal server performance and security.",
-          "SSL Certificates: Ensuring secure data transmission with SSL certificates for enhanced trust.",
-        ],
-      },
-      MediaComponent: {
-        mediaType: "image",
-        imageUrl: HostingCover,
-      },
-    },
-    // Add more grid data objects as needed
-  ];
-
-
-
-  const isMobileDevice = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    window.navigator.userAgent
-  );
-
   return (
     <>
       <Banner />
-      <ComponentGrid>
-        {gridData.map((item, index) => (
-          <React.Fragment key={index}>
-            {isMobileDevice ? (
-              // Render CustomComponent and MediaComponent in the same order for mobile devices
-              <>
-                <CustomComponent {...item.CustomComponent} />
-                <MediaComponent {...item.MediaComponent} />
-              </>
-            ) : (
-              // Alternate the order of rendering for non-mobile devices
-              index % 2 === 0 ? (
-                <>
-                  <CustomComponent {...item.CustomComponent} />
-                  <MediaComponent {...item.MediaComponent} />
-                </>
-              ) : (
-                <>
-                  <MediaComponent {...item.MediaComponent} />
-                  <CustomComponent {...item.CustomComponent} />
-                </>
-              )
-            )}
-          </React.Fragment>
-        ))}
-      </ComponentGrid>
     </>
   );
 }
