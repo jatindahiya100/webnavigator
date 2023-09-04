6faef5992a3cbe1340b9b84be8597abad47e36c4
Jatin Dahiya <jatindahiya100@gmail.com>
2023-09-04 14:04:26 +0530

    Update Changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import '../assets/css/Banner.css'
+import React from 'react';
+import '../assets/css/Banner.css';
 import Lottie from 'lottie-react';
 import Lottie01 from '../assets/images/Lottie/Landing-Page_Lottie-01.json';
 
@@ -8,22 +8,34 @@ export default function Banner() {
         loop: true,
         autoplay: true,
         animationData: Lottie01,
-    }
+    };
+
     return (
-        <div className='h-screen w-full bg-[url(./assets/images/bg-gradient1.jpg)] bg-cover bg-no-repeat bg-center'>
-            <section className='max-w-screen-xl  mx-auto my-0 h-full grid grid-cols-2 gap-4 justify-center items-center'>
-                <div className='flex flex-col gap-y-8'>
-                    <span className='text-lg font-bold text-blue-600'>Navigating the Web's Waves with Ease</span>
-                    <h1 className='text-7xl font-extrabold capitalize'>Welcome To The Web Navigators</h1>
-                    <p className='text-lg text-slate-700'>Welcome to The Web Navigators, where we bring your digital dreams to life with a dedicated team, empowering your brand online.</p>
-                    <div className="call-to-actions">
-                        <button className='bg-blue-500 px-6 py-4 rounded-lg text-white'>Get Started</button>
+        <div className='bg-[url(./assets/images/bg-gradient1.jpg)] bg-cover bg-no-repeat bg-center min-h-screen flex items-center'>
+            <section className='max-w-screen-xl mx-auto p-4 sm:p-8'>
+                <div className='grid grid-cols-1 gap-4 justify-center items-center md:grid-cols-2'>
+                    <div className='text-center sm:text-left'>
+                        <span className='text-lg font-bold text-blue-600'>
+                            Navigating the Web's Waves with Ease
+                        </span>
+                        <h1 className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold capitalize mt-4'>
+                            Welcome To The Web Navigators
+                        </h1>
+                        <p className='text-lg text-slate-700 mt-4'>
+                            Welcome to The Web Navigators, where we bring your digital dreams
+                            to life with a dedicated team, empowering your brand online.
+                        </p>
+                        <div className='mt-6'>
+                            <button className='bg-blue-500 px-6 py-4 rounded-lg text-white'>
+                                Get Started
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <Lottie animationData={lottieOptions.animationData} />
+                    <div className='mt-4 md:mt-0'>
+                        <Lottie animationData={lottieOptions.animationData} />
+                    </div>
                 </div>
             </section>
         </div>
-    )
+    );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,75 @@
-import React from 'react'
-import '../assets/css/Header.css'
-import { Link, NavLink } from 'react-router-dom'
-import Logo from '../assets/images/logo.png'
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom'; // Make sure you import Link and NavLink
+import Logo from '../assets/images/logo.png';
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
     return (
-        <header>
-            <div className="logo">
-                <Link to='/'>
-                    <img src={Logo} alt="The Web Navigators Logo" />
-                    <span>The Web Navigators</span>
-                </Link>
+        <div className="relative">
+            {/* Header for Smaller Screens */}
+            <header className='md:hidden flex items-center justify-between h-20 bg-transparent px-4'>
+                <div className="logo">
+                    <Link to='/' className='inline-flex items-center gap-x-2.5 font-semibold'>
+                        <img className='h-9 w-9' src={Logo} alt="The Web Navigators Logo" />
+                        <span>The Web Navigators</span>
+                    </Link>
+                </div>
+                <div className="hamburger">
+                    <button
+                        onClick={toggleMenu}
+                        className='text-2xl text-gray-500 focus:outline-none'
+                    >
+                        <svg viewBox="0 0 24 24" className="h-6 w-6 stroke-slate-900">
+                            <path d="M3.75 12h16.5M3.75 6.75h16.5M3.75 17.25h16.5" fill="none" strokeWidth="1.5" strokeLinecap="round"></path>
+                        </svg>
+                    </button>
+                </div>
+            </header>
+
+            {/* Full-screen Side Navigation Menu */}
+            <div className={`fixed top-0 right-0 h-screen w-72 bg-white shadow-lg overflow-y-auto transition-transform transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} md:translate-x-0 md:w-0 z-50`}>
+                <button
+                    onClick={closeMenu}
+                    className='md:hidden text-2xl text-gray-500 focus:outline-none absolute top-4 right-4'
+                >
+                    <svg viewBox="0 0 24 24" className="h-6 w-6 stroke-slate-900">
+                        <path d="M6 18L18 6M6 6l12 12" fill=" none " strokeWidth="1.5" strokeLinecap="round"></path>
+                    </svg>
+                </button>
+                <nav className='flex flex-col items-center gap-4 py-4'>
+                    <NavLink onClick={closeMenu} className='cursor-pointer text-base font-semibold leading-6 font-normal text-slate-900 hover:bg-gray-200 py-2 px-4 transition duration-600' to='/'>Home</NavLink>
+                    <NavLink onClick={closeMenu} className='cursor-pointer text-base font-semibold leading-6 font-normal text-slate-900 hover:bg-gray-200 py-2 px-4 transition duration-600' to='/blogs'>Blogs</NavLink>
+                    <NavLink onClick={closeMenu} className='cursor-pointer text-base font-semibold leading-6 font-normal text-slate-900 hover:bg-gray-200 py-2 px-4 transition duration-600' to='work'>Previous Work</NavLink>
+                    <NavLink onClick={closeMenu} className='cursor-pointer text-base font-semibold leading-6 font-normal text-slate-900 hover:bg-gray-200 py-2 px-4 transition duration-600' to='what-we-do'>What We Do ?</NavLink>
+                    <NavLink onClick={closeMenu} className='cursor-pointer text-base font-semibold leading-6 font-normal text-slate-900 hover:bg-gray-200 py-2 px-4 transition duration-600' to='start-project'>Start Project</NavLink>
+                </nav>
             </div>
 
-            <nav>
-                <ul>
-                    <li><NavLink className='' to='/'>Home</NavLink></li>
-                    <li><NavLink className='' to='/blogs'>Blogs</NavLink></li>
-                    <li><NavLink className='' to='work'>Previous Work</NavLink></li>
-                    <li><NavLink className='' to='what-we-do'>What We Do ?</NavLink></li>
-                    <li><NavLink to='start-project'>Start Project</NavLink></li>
-                </ul>
-            </nav>
-        </header>
-    )
+            {/* Desktop Header */}
+            <header className='mx-auto max-w-screen-xl w-full flex flex-row items-center justify-between h-20 bg-transparent absolute top-0 left-0 right-0 md:flex md:flex-row md:items-center md:justify-between md:h-20 bg-transparent hidden'>
+                <div className="logo">
+                    <Link to='/' className='inline-flex items-center gap-x-2.5 font-semibold'>
+                        <img className='h-9 w-9' src={Logo} alt="The Web Navigators Logo" />
+                        <span>The Web Navigators</span>
+                    </Link>
+                </div>
+                <nav className='hidden md:flex items-center gap-12'>
+                    <NavLink className='cursor-pointer text-base font-semibold leading-6 font-normal rounded-lg text-slate-900 py-2 px-4 hover:bg-gray-200 transition duration-600' to='/'>Home</NavLink>
+                    <NavLink className='cursor-pointer text-base font-semibold leading-6 font-normal rounded-lg text-slate-900 py-2 px-4 hover:bg-gray-200 transition duration-600' to='/blogs'>Blogs</NavLink>
+                    <NavLink className='cursor-pointer text-base font-semibold leading-6 font-normal rounded-lg text-slate-900 py-2 px-4 hover:bg-gray-200 transition duration-600' to='work'>Previous Work</NavLink>
+                    <NavLink className='cursor-pointer text-base font-semibold leading-6 font-normal rounded-lg text-slate-900 py-2 px-4 hover:bg-gray-200 transition duration-600' to='what-we-do'>What We Do ?</NavLink>
+                    <NavLink className='cursor-pointer text-base font-semibold leading-6 font-normal rounded-lg text-slate-900 py-2 px-4 hover:bg-gray-200 transition duration-600' to='start-project'>Start Project</NavLink>
+                </nav>
+            </header>
+        </div>
+    );
 }
