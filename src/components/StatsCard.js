178729bb6c47fa079b787bac32623cb8a61574c4
Jatin Dahiya <jatindahiya100@gmail.com>
2023-09-06 23:52:00 +0530

    Update Changes

--- src/components/StatsCard.js
+++ src/components/StatsCard.js
@@ -5,34 +5,38 @@ const StatsCard = () => {
         <div className="container mx-auto px-4 sm:px-0">
             <div className="border-2 rounded-lg p-6">
                 <h2 className="text-3xl font-semibold mb-8 text-slate-900 text-center mt-12" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">Trusted by businesses worldwide</h2>
-                <p className='text-center text-slate-500' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">We're proud to share our achievements in serving clients,
-                    completing projects, and growing our dedicated team.</p>
+                <p className='text-center text-slate-500' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">We're proud to share our achievements in serving clients, completing projects, and growing our dedicated team.</p>
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-12">
                     {/* Client Stats Card */}
-                    <div className="p-6 rounded-lg text-center bg-slate-100 transition duration-300 transform hover:scale-105" data-aos="zoom-out-left">
-                        <div className="text-3xl font-bold mt-2 text-slate-900">35+</div>
-                        <div className="text-sm font-bold text-gray-500">Clients</div>
+                    <div className="p-6 rounded-lg text-center bg-slate-100 transition duration-300 transform hover:scale-105">
+                        <div className="h-24 overflow-hidden">
+                            <div className="text-3xl font-bold mt-2 text-slate-900">35+</div>
+                            <div className="text-sm font-bold text-gray-500">Clients</div>
+                        </div>
                     </div>
 
                     {/* Projects Completed Stats Card */}
-                    <div className="p-6 rounded-lg text-center bg-slate-100 transition duration-300 transform hover:scale-105" data-aos="zoom-out-left">
-                        <div className="text-3xl font-bold mt-2 text-slate-900">12+</div>
-                        <div className="text-sm font-bold text-gray-500">Projects Completed</div>
-
+                    <div className="p-6 rounded-lg text-center bg-slate-100 transition duration-300 transform hover:scale-105">
+                        <div className="h-24 overflow-hidden">
+                            <div className="text-3xl font-bold mt-2 text-slate-900">12+</div>
+                            <div className="text-sm font-bold text-gray-500">Projects Completed</div>
+                        </div>
                     </div>
 
                     {/* Team Members Stats Card */}
-                    <div className="p-6 rounded-lg text-center bg-slate-100 transition duration-300 transform hover:scale-105" data-aos="zoom-out-left">
-                        <div className="text-3xl font-bold mt-2 text-slate-900">10+</div>
-                        <div className="text-sm font-bold text-gray-500">Team Members</div>
-
+                    <div className="p-6 rounded-lg text-center bg-slate-100 transition duration-300 transform hover:scale-105">
+                        <div className="h-24 overflow-hidden">
+                            <div className="text-3xl font-bold mt-2 text-slate-900">10+</div>
+                            <div className="text-sm font-bold text-gray-500">Team Members</div>
+                        </div>
                     </div>
 
-                    {/* Countries Stats Card */}
-                    <div className="p-6 rounded-lg text-center bg-slate-100 transition duration-300 transform hover:scale-105" data-aos="zoom-out-left">
-                        <div className="text-3xl font-bold mt-2 text-slate-900">50k+</div>
-                        <div className="text-sm font-bold text-gray-500">Revenue</div>
-
+                    {/* Revenue Stats Card */}
+                    <div className="p-6 rounded-lg text-center bg-slate-100 transition duration-300 transform hover:scale-105">
+                        <div className="h-24 overflow-hidden">
+                            <div className="text-3xl font-bold mt-2 text-slate-900">50k+</div>
+                            <div className="text-sm font-bold text-gray-500">Revenue</div>
+                        </div>
                     </div>
                 </div>
             </div>
