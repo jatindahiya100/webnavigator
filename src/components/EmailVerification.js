85e50131339afbd3eda739c5e274e0d3f722c327
Jatin Dahiya <jatindahiya100@gmail.com>
2023-09-08 09:30:13 +0530

    Update Changes

--- src/components/EmailVerification.js
+++ src/components/EmailVerification.js
@@ -61,8 +61,8 @@ function EmailVerification() {
                     {isLoading ? (
                         <div className="animate-spin h-5 w-5 border-t-2 border-b-2 border-white rounded-full"></div>
                     ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                     )}
                 </button>
